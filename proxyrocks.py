474811e5b95e45499fe870ceb585f47bf660515c
Shaohua <cell.fantasy@qq.com>
2019-03-17 14:51:37 +0700

    Update proxyrocks.py

--- proxyrocks.py
+++ proxyrocks.py
@@ -196,8 +196,9 @@ def base64_decode(base64_encode_str):
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run().getList()
+    db.create_all()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True).getList()
     print("finished")    
 
 
